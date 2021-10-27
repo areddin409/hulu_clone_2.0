6f3b2eb3d47cb5417f57c2035473602643eb39b6
Andrew Reddin <areddin409@gmail.com>
2021-10-27 11:11:46 -0500

    created layout and pulling movie data from tmdb

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import NavBar from '../components/NavBar';
+import Results from '../components/Results';
+import requests from '../utils/requests';
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div className=''>
       <Head>
@@ -10,8 +13,24 @@ export default function Home() {
       </Head>
 
       <Header />
-      {/* NavBar */}
-      {/* Results */}
+      <NavBar />
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results
+    }
+  };
 }
